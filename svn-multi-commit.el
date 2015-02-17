7fb2ef7fac71c92e6d8134e4014ef49b84e0776e
Steven Rémot <steven.remot@inovia-team.com>
2015-02-17 14:26:03 +0100

    Write log message in a buffer instead of the minibuffer

--- svn-multi-commit.el
+++ svn-multi-commit.el
@@ -56,6 +56,10 @@
 (defvar svn-multi-commit--files '()
   "Files to commit.")
 
+(defconst svn-multi-commit--log-buffer-name "*svn-multi-commit-log*")
+
+(defconst svn-multi-commit--summary-buffer-name " *svn-multi-commit-files*")
+
 ;;;###autoload
 (defun svn-multi-commit-add ()
   "Add the selected files to commit in mode `vc-mode'."
@@ -78,19 +82,54 @@
 You will be asked to confirm you want to commit *these* files.
 Then you can enter a commit message in the mini buffer."
   (interactive)
-  (with-temp-buffer
-    (insert "Files to commit :\n\n")
-    (dolist (file svn-multi-commit--files)
-      (insert (format "- %s\n" file)))
+  (let ((log-buffer (svn-multi-commit--create-log-buffer))
+        (summary-buffer (svn-multi-commit--create-summary-buffer)))
+    (pop-to-buffer log-buffer)
+    (display-buffer summary-buffer 'display-buffer-other-window)))
 
-    (switch-to-buffer-other-window (current-buffer))
+(defun svn-multi-commit--create-log-buffer ()
+  "Create the buffer in which log message will be written."
+  (let ((buffer (get-buffer-create svn-multi-commit--log-buffer-name)))
+    (with-current-buffer buffer
+      (erase-buffer)
+      (insert svn-multi-commit-template)
+      (svn-multi-commit-log-mode))
+    buffer))
 
-    (when (yes-or-no-p "Do you want to do a commit with these files? ")
-      (let* ((message (read-from-minibuffer "Commit message: " svn-multi-commit-template))
-             (args (append (list "ci"  "-m" message) svn-multi-commit--files)))
-        (apply 'start-process "svn-multi-ci" "*ubeeqo-svn-ci*" "svn" args)
-        (svn-multi-commit-reset)))))
+(defun svn-multi-commit--create-summary-buffer ()
+  "Create the buffer that displays files to commit."
+  (let ((buffer (get-buffer-create svn-multi-commit--summary-buffer-name)))
+    (with-current-buffer buffer
+      (erase-buffer)
+      (insert "Files to commit :\n\n")
+      (dolist (file svn-multi-commit--files)
+        (insert (format "- %s\n" file))))
+    buffer))
 
+(defun svn-multi-commit-log-mode-send ()
+  "Commit the files registered with `svn-multi-commit-add'.
+
+Use the buffer content as log message."
+  (interactive)
+  (let* ((message (buffer-substring-no-properties (point-min) (point-max)))
+         (args (append (list "ci"  "-m" message) svn-multi-commit--files)))
+    (apply 'start-process "svn-multi-ci" "*ubeeqo-svn-ci*" "svn" args)
+    (svn-multi-commit-reset)
+    (svn-multi-commit-log-mode-close)))
+
+(defun svn-multi-commit-log-mode-close ()
+  "Close log windows."
+  (interactive)
+  (kill-buffer svn-multi-commit--log-buffer-name)
+  (kill-buffer svn-multi-commit--summary-buffer-name))
+
+(define-derived-mode svn-multi-commit-log-mode fundamental-mode "SVN MC Log"
+  "Major mode for writing log message for svn-multi-commit.
+
+\\{svn-multi-commit-log-mode-map}")
+
+(define-key svn-multi-commit-log-mode-map (kbd "C-c C-c") #'svn-multi-commit-log-mode-send)
+(define-key svn-multi-commit-log-mode-map (kbd "C-c C-k") #'svn-multi-commit-log-mode-close)
 
 (provide 'svn-multi-commit)
 
